72829add3e2855ef2f6a3db70339a13751c0cceb
sathishkumar <smanodevi@gmail.com>
2021-10-04 16:53:01 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+          Github Finder
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
