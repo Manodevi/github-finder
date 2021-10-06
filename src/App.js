f49caeb6c3eeefc048cc92f625314fa372b2fe1b
sathishkumar <smanodevi@gmail.com>
2021-10-06 10:42:54 +0400

    use axios to make request to github api and store the users data in a users state and pass it to Users Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import Navbar from './layout/Navbar';
-import Users from './components/users/Users.js'
+import Users from './components/users/Users.js';
+import axios from 'axios';
 
 class App extends React.Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const axiosRes = await axios.get("https://api.github.com/users");
+    this.setState({ users: axiosRes.data, loading: false });
+  }
+
   render () {
     return (
       <div className="App">
@@ -10,7 +22,7 @@ class App extends React.Component {
           <Navbar title="Github Finder" />
         </header>
         <div className="container">
-          <Users />
+          <Users users={this.state.users} loading={this.state.loading} />
         </div>
       </div>
     );
