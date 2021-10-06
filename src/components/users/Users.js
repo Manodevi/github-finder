3dff11f957441a4c4e297dd9428933129b5e6524
sathishkumar <smanodevi@gmail.com>
2021-10-06 10:46:00 +0400

    Removed hardcoded users array and retrieve users data as props from App.js state

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,41 +1,19 @@
 import React, { Component } from 'react';
 import UserItem from './UserItem';
+import Spinner from '../../layout/Spinner';
 
-class Users extends Component {
-  state = {
-    users: [
-      {
-        id: 1,
-        login: 'mojombo',
-        avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-      },
-      {
-        id: 2,
-        login: 'defunkt',
-        avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
-        html_url: 'https://github.com/defunkt'
-      },
-      {
-        id: 3,
-        login: 'pjhyett',
-        avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
-        html_url: 'https://github.com/pjhyett'
-      },
-      {
-        id: 4,
-        login: 'wycats',
-        avatar_url: 'https://avatars.githubusercontent.com/u/4?v=4',
-        html_url: 'https://github.com/wycats'
-      },
-    ]
-  }
+class Users extends Component {  
   render () {
-    return <div className="grid-3">
-      {this.state.users.map(user => (
-        <UserItem key={user.id} user={user} />
-      ))}
-    </div>;
+    const { loading, users } = this.props;
+    if(loading) {
+      return <Spinner />;
+    } else {
+      return <div className="grid-3">
+        {this.props.users.map(user => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>;
+    }
   }
 }
 
