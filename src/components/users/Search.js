70a82e2ce698e23dd652fb67cc161d6fe6f49f0a
sathishkumar <smanodevi@gmail.com>
2021-10-06 19:02:32 +0400

    When submitting empty data in search, adding alert message and type to app.js

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,33 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
 
 class Search extends Component {
   state = {
     text: ''
   };
 
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearSearch: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
+  };
+
   onchange = e => this.setState({ [e.target.name]: e.target.value });
 
   onSubmit(e) {
     e.preventDefault();
-    console.log(this.state.text);
+    if(this.state.text === '') {
+      this.props.setAlert('Please enter something...', 'light');
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: '' });
+    }
   }
 
   render() {
     return (
-      <form onSubmit={this.onSubmit.bind(this)} className="form">
-        <input 
-          type="text"
-          name="text"
-          placeholder="Search users..."
-          value={this.state.text}
-          onChange={this.onchange}
-        />
-        <input
-          type="submit"
-          value="Search"
-          className="btn btn-dark btn-block"
-        />
-      </form>
+      <Fragment>
+        <form onSubmit={this.onSubmit.bind(this)} className="form">
+          <input 
+            type="text"
+            name="text"
+            placeholder="Search users..."
+            value={this.state.text}
+            onChange={this.onchange}
+          />
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-dark btn-block"
+          />
+        </form>
+        {this.props.showClear && (
+          <button 
+            className="btn btn-light btn-block"
+            onClick={this.props.clearSearch}
+            >Clear
+          </button>
+        )}
+      </Fragment>
     );
   }
 }
