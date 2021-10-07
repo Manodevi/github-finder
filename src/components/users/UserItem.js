6718be94092ab148c4d4b9a713e9082d56b3bdc2
sathishkumar <smanodevi@gmail.com>
2021-10-07 17:44:37 +0400

    Added dynamic url for each user - to view their page

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class UserItem extends Component {
   static propTypes = {
     user: PropTypes.object.isRequired
   }
   render () {
-    const { login, avatar_url, html_url } = this.props.user;
+    const { login, avatar_url } = this.props.user;
 
     return <div className="card text-center">
       <img src={avatar_url} alt={login} className="round-img" />
       <h3>{login}</h3>
       <div>
-        <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
+        <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">More</Link>
       </div>
     </div>
   }
