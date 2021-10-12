6778d0e1aa3798a26fb5a07eacef0c668856b570
sathishkumar <smanodevi@gmail.com>
2021-10-12 11:10:45 +0400

    COnverting Class Component to functional component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react';
 import UserItem from './UserItem';
 import PropTypes from 'prop-types';
 import Spinner from '../../layout/Spinner';
 
-class Users extends Component {  
-  static propTypes = {
-    users: PropTypes.array.isRequired,
-    loading: PropTypes.bool.isRequired
+const Users = ({ loading, users }) => {  
+  if(loading) {
+    return <Spinner />;
+  } else {
+    return <div className="grid-3">
+      {users.map(user => (
+        <UserItem key={user.id} user={user} />
+      ))}
+    </div>;
   }
-  render () {
-    const { loading, users } = this.props;
-    if(loading) {
-      return <Spinner />;
-    } else {
-      return <div className="grid-3">
-        {users.map(user => (
-          <UserItem key={user.id} user={user} />
-        ))}
-      </div>;
-    }
-  }
+}
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
 }
 
 export default Users;
